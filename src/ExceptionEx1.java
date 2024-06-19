caeab604bc048de2dbe7fc227cb8ada89affacdf
Aluno <Aluno@10.85.198.10>
2024-06-19 15:53:43 -0300

    ExceptionEx1

---
+++ src/ExceptionEx1.java
@@ -0,0 +1,15 @@
+public class ExceptionEx1 {
+
+    public static void main(String args[]) {
+
+        int[] nums = {3, 2, 6, 1};
+        System.out.println(nums);
+        int total = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = nums[i];
+            
+        }
+         System.out.println(total);
+    }
+}
